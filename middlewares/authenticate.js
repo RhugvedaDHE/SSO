7beba2373826278dcf132ae769a57ffe085e14ac
Rhugveda Desai <dherhugveda@gmail.com>
2023-04-24 15:36:11 +0530

    added some comments

--- middlewares/authenticate.js
+++ middlewares/authenticate.js
@@ -5,9 +5,10 @@ module.exports = (req, res, next) => {
         if (err) return next(err);
 
         if (!user) return res.status(401).json({message: "Unauthorized Access - No Token Provided!"});
-
+        
         req.user = user;
-
+        console.log("********************************************************************************")
+        console.log("", req.user)
         next();
 
     })(req, res, next);
