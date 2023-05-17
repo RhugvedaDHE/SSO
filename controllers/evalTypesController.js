eacf2698d8eb6dfc3bad87082751ceb0322956a5
RhugvedaDHE <dherhugveda@gmail.com>
2023-05-17 12:56:02 +0530

    get eval types done

--- controllers/evalTypesController.js
+++ controllers/evalTypesController.js
@@ -1,5 +1,4 @@
 const express = require("express");
-const District = require("../models").District;
 const EvalTypes = require("../models").EvalTypes;
 const { success, errorResponse, validation } = require("../responseApi");
 
@@ -17,14 +16,15 @@ exports.create = function (req, res) {
 
 exports.get = async function (req, res) {
   await EvalTypes.findAll({
-    // where: {
-    //   is_active: true,
-    // },
+    where: {
+      is_active: true,
+    },
   })
     .then((evalTypes) => {
-      res.status(200).json(success("Evaluation Types fetched successfully!", cities));
+      res.status(200).json(success("Evaluation Types fetched successfully!", evalTypes));
     })
     .catch((error) => {
+      console.log(error);
       res.status(400).json(errorResponse(error, 400));
     });
 };
